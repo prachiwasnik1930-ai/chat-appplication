3471bce87b2c92cc18847ba704576c563d71235a
Prach19 <prachiwasnik1930@gmail.com>
2026-06-14 16:55:06 +0530

    Fix socket URL

--- frontened/src/socket.js
+++ frontened/src/socket.js
@@ -1,5 +1,4 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
-
+const socket = io("https://chat-application-production.up.railway.app");
 export default socket;
